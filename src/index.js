b33d00615805527adcd3cc24d557366c0e505010
ritz-w <ritz.wu@outlook.com>
2018-09-20 13:54:35 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => console.log("great time!")} />
   </div>,
   document.getElementById('root')
 );
